7fd79224aebbfa6752125879386c5f379cd97d17
Angie <angie.bikou@yahoo.com>
2021-02-03 22:30:30 +0100

    Remplacement des attributs privés par des attributs publics

--- Roue.py
+++ Roue.py
@@ -1,26 +1,26 @@
 class Roue(object):
     def __init__(self):
-        self.__moteurAllume=False
-        self.__vitesseRotation=0.
+        self.moteurAllume=False
+        self.vitesseRotation=0.
     def setVitesseRotation(self,vitesse):
         """
         float
         Permet de changer la vitesse du moteur
         """
-        self.__vitesseRotation=vitesse
+        self.vitesseRotation=vitesse
         if vitesse==0. :
-            self.__moteurAllume=False
+            self.moteurAllume=False
         else:
-            self.__moteurAllume=True
+            self.moteurAllume=True
     def get_moteurAllume(self):
         """"
         Retourne la valeur de moteurAllume
         """
-        return self.__moteurAllume
+        return self.moteurAllume
     def get_vitesseRotation(self):
         """
         Retourne la valeur de vitesseRotation
         """
-        return self.__vitesseRotation
+        return self.vitesseRotation
 
     
