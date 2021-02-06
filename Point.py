866fdfee976eaf6688232370a756e30feb57c42a
Angie <angie.bikou@yahoo.com>
2021-02-06 22:34:09 +0100

    Ajout de la méthode distance

--- Point.py
+++ Point.py
@@ -1,8 +1,19 @@
+import math
+
 class Point:
 	"Classe permettant de définir des points"
 
 	def __init__(self,x,y):
 		self.x = x
 		self.y = y
+
+	def distance(self, point):
+		"""
+		Point -> float
+		Retourne la distance entre le point courant et le point en parametre.
+		"""
+
+		return math.sqrt((point.x - self.x) * (point.x - self.x) + (point.y - self.y) * (point.y - self.y))
+
 
 
