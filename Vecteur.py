cf475411e0155bc4dcf0ecac7c20b0c767f6f737
Martin <martin.billon@icloud.com>
2021-02-06 21:34:45 +0100

    méthode calculNorme

--- Vecteur.py
+++ Vecteur.py
@@ -4,5 +4,7 @@ class Vecteur:
 	def __init__(self,point1,point2):
 		self.point1 = point1
 		self.point2 = point2
-
-
+    def calculNorme(self):
+        "Calcule la norme du vecteur"
+        return sqrt(pow(self.point2.x-self.point1.x,2)+pow(self.point2.y-self.point1.y))
+    
