7fb630ba02a065dd7a265d35a6e204e78be1480c
PtiBaka <61068341+PtiBaka@users.noreply.github.com>
2021-02-06 20:41:01 +0100

    Add files via upload

---
+++ Vecteur.py
@@ -0,0 +1,8 @@
+class Vecteur:
+	"Classe vecteur permettant de définir les déplacements de notre futur robot"
+
+	def __init__(self,point1,point2):
+		self.point1 = point1
+		self.point2 = point2
+
+
