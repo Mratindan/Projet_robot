372bd90bf3961a51e4fbe4b3ab24bb8b21985906
Oucible <ouciblec@gmail.com>
2021-02-07 14:37:44 +0100

    Update Roue.py

--- Roue.py
+++ Roue.py
@@ -4,29 +4,5 @@ class Roue(object):
         self.moteurAllume=False
         self.vitesseRotation=0.
 
-    def setVitesseRotation(self,vitesse):
-        """
-        float --> None
-        Permet de changer la vitesse du moteur
-        """
-        self.vitesseRotation=vitesse
-        if vitesse==0. :
-            self.moteurAllume=False
-        else:
-            self.moteurAllume=True
-
-    def get_moteurAllume(self):
-        """"
-        None -> boolean
-        Retourne la valeur de moteurAllume
-        """
-        return self.moteurAllume
-
-    def get_vitesseRotation(self):
-        """
-        None -> float
-        Retourne la valeur de vitesseRotation
-        """
-        return self.vitesseRotation
 
     
