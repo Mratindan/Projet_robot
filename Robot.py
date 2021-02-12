ccddb04b7815fbe17cc3b69d045c82f7be34bd68
Martin <martin.billon@icloud.com>
2021-02-12 16:30:01 +0100

    methode changePosition finie

--- Robot.py
+++ Robot.py
@@ -8,10 +8,11 @@ class Robot :
     def __init__(self, a, b):
         print("Creation d'un robot")
         self.position = Point(a, b)
+        self.positionNorme=0.
         self.vitesse = 0.
         self.acceleration=0.
-        self.positionVecteur=Vecteur(0,0)
-        self.vitesseVecteur=Vecteur(0,0)
+        self.positionVecteur=Vecteur(Polynome(0,0,1),Polynome(0,0,1))
+        self.vitesseVecteur=Vecteur(Polynome(0,0,0),Polynome(0,0,0))
         self.accelerationVecteur=Vecteur(0,0)
         self.positionVecteurTemps=Vecteur(0,0)
         self.vitesseVecteurTemps=Vecteur(0,0)
@@ -35,12 +36,20 @@ class Robot :
     def changePosition(self,acceleration,angle,temps):
         """
         float*float->None
-        change la fonction caractérisant le mouvement du robot
+        change le vecteur mouvement du robot
+        l'angle est lu dans le sens des aiguilles d'une montre
         """
-        self.position.x=self.positionVecteur.x.calcul(temps)
-        self.position.y=self.positionVecteur.y.calcul(temps)
+        self.position.x=self.positionVecteur.point1.calcul(temps)
+        self.position.y=self.positionVecteur.point2.calcul(temps)
         self.positionVecteurTemps=Vecteur(self.position.x,self.position.y)
-        #faire classe polynome avec methode calcul en fonction du temps
-        
-        
+        self.positionNorme=math.sqrt(pow(self.positionVecteurTemps.point1,2)+pow(self.positionVecteurTemps.point2,2))
+        self.vitesseVecteurTemps=Vecteur(self.vitesseVecteur.point1.calcul(temps),self.vitesseVecteur.point2.calcul(temps))
+        self.vitesse=math.sqrt(pow(self.vitesseVecteurTemps.point1,2)+pow(self.vitesseVecteurTemps.point2,2))
+        self.acceleration=acceleration
+        self.accelerationVecteur.point1=(acceleration/self.positionNorme)*((self.positionVecteurTemps.point1)*math.cos(angle)+(self.positionVecteurTemps.point2)*math.sin(angle))
+        self.accelerationVecteur.point2=(acceleration/self.positionNorme)*(-(self.positionVecteurTemps.point1)*math.sin(angle)+(self.positionVecteurTemps.point2)*math.cos(angle))
+        #utiliser methode rotation de la classe vecteur? (tester d'abord) -> pas de changment de norme
+        self.vitesseVecteur=Vecteur(Polynome(0,self.accelerationVecteur.point1,self.vitesseVecteurTemps.point1),Polynome(0,self.accelerationVecteur.point2,self.vitesseVecteurTemps.point2))
+        self.positionVecteur=Vecteur(Polynome(0.5*self.accelerationVecteur.point1,self.vitesseVecteurTemps.point1,self.positionVecteurTemps.point1),Polynome(0.5*self.accelerationVecteur.point2,self.vitesseVecteurTemps.point2,self.positionVecteurTemps.point2))
+        #creer une methode pour transformer le vecteur position en fonction utilisable
         
