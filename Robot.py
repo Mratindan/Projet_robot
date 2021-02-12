5e89ed8bb047c772097060a4a037581c17884f94
Martin <martin.billon@icloud.com>
2021-02-12 15:01:13 +0100

    nouveaux attributs et methode changePosition

--- Robot.py
+++ Robot.py
@@ -1,4 +1,6 @@
+import math
 from Point import Point
+from Vecteur import Vecteur
 
 class Robot :
     "Definition d'un robot"
@@ -6,8 +8,13 @@ class Robot :
     def __init__(self, a, b):
         print("Creation d'un robot")
         self.position = Point(a, b)
-        self.direction = 0.
         self.vitesse = 0.
+        self.acceleration=0.
+        self.positionVecteur=Vecteur(0,0)
+        self.vitesseVecteur=Vecteur(0,0)
+        self.accelerationVecteur=Vecteur(0,0)
+        self.positionVecteurTemps=Vecteur(0,0)
+        self.vitesseVecteurTemps=Vecteur(0,0)
         
     def deplace(self, dx, dy):
         """
@@ -25,4 +32,15 @@ class Robot :
         """
         print("Position du robot [", self.position.x, ",", self.position.y, "]")
             
-
+    def changePosition(self,acceleration,angle,temps):
+        """
+        float*float->None
+        change la fonction caractérisant le mouvement du robot
+        """
+        self.position.x=self.positionVecteur.x.calcul(temps)
+        self.position.y=self.positionVecteur.y.calcul(temps)
+        self.positionVecteurTemps=Vecteur(self.position.x,self.position.y)
+        #faire classe polynome avec methode calcul en fonction du temps
+        
+        
+        
