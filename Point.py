5aa13e370011643e3c0a19a9746732e567287459
PtiBaka <antony@DESKTOP-95NF774.localdomain>
2021-02-14 19:15:58 +0100

    ajout de getteurs X et Y dans la classe Point

--- Point.py
+++ Point.py
@@ -12,8 +12,21 @@ class Point:
 		Point -> float
 		Retourne la distance entre le point courant et le point en parametre.
 		"""
-
 		return math.sqrt((point.x - self.x) * (point.x - self.x) + (point.y - self.y) * (point.y - self.y))
+	
+	def getX(self):
+		return self.x
+
+	def getY(self):
+		return self.y
 
 
+point1 = Point(1,2)
+point2 = Point(4,6)
 
+point3 = Point(-2,-1)
+point4 = Point(6,-3)
+
+
+print("Distance entre les points (1,2) et (4,6) = ", point1.distance(point2))
+print("Distance entre les points (-2,-1) et (6,-3) = ", point4.distance(point3))
