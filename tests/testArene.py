d6223230fc59167dfc89ec83fa9bbe6a5af50430
Aryj Takali <takaliaryj@gmail.com>
2021-02-17 10:52:19 +0100

    ajouter des tests unitaires

--- tests/testArene.py
+++ tests/testArene.py
@@ -1,19 +1,14 @@
 import unittest
-
+import sys
+sys.path.append("..")
 from Projet_robot.Arene import Arene
+from Projet_robot.Point import Point
 
 class TestArene(unittest.TestCase):
     def test_arene_is_created(self):
         arene = Arene(10,15)
         self.assertIsInstance(arene,Arene)
 
-    def test_est_vide(self):
-        self.assert_(True,"est vide")
-
 
 if __name__ =='__main__':
     unittest.main()
-
-
-
-
