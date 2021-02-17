4df618ba0122fb7ee2f1684d52770746cdf5605d
Angie <angie.bikou@yahoo.com>
2021-02-17 09:14:50 +0100

    Réorganisation de l'arborescence

--- Obstacle.py
+++
@@ -1,8 +0,0 @@
-from Point import Point 
-
-class Obstacle:
-    "Definition de la classe Obstacle"
-
-
-def __init__(self, abscisse, ordonnee):
-    self.position = Point(abscisse, ordonnee)
--- Roue.py
+++
@@ -1,5 +0,0 @@
-class Roue(object):
-
-    def __init__(self):
-        self.moteurAllume=False
-        self.vitesseRotation=0.
