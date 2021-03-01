2b57dc1999247fb1601c70580bb540b542c45120
Angie <angie.bikou@yahoo.com>
2021-03-01 21:44:01 +0100

    Les boutons play et stop sont fonctionnels, la touche entrée est liée à play et espace à stop

--- gui/viewer.py
+++ gui/viewer.py
@@ -4,6 +4,13 @@ from tkinter import ttk
 class Viewer :
 
     def __init__(self, simulation):
+        """
+        simulation : Tk
+        Initialise une fenêtre graphique 
+        """
+
+        # Variables nécessaires pour faire tourner la simulation
+        self.after_id = None
 
         # Création de la fenêtre principale de l'application
         simulation.title("Gopigo Simulator")
@@ -15,16 +22,21 @@ class Viewer :
         # Le cadre principale qui contiendra la toile, les boutons etc
         cadre = ttk.Frame(simulation)
         # La toile dans laquelle sera dessinée la simulation
-        self.terrain = Canvas(cadre, borderwidth = 2, relief = 'ridge', width = 600, height = 600, background = "white")
+        self.arene = Canvas(cadre, borderwidth = 2, relief = 'ridge', width = 600, height = 600, background = "white")
         # Les boutons
-        play = ttk.Button(cadre, text = "Play")
-        stop = ttk.Button(cadre, text = "Stop")
+        play = ttk.Button(cadre, text = "Play", command = self.run)
+        stop = ttk.Button(cadre, text = "Stop", command = self.stop)
+        # Association de certaines touches du clavier à des commandes (en alternative aux boutons)
+        simulation.bind("<Return>", lambda e: play.invoke())
+        simulation.bind("<space>", lambda e: stop.invoke())
+
 
         # Placement des widgets
         cadre.grid(column = 0, row = 0, sticky = (N, S, E, W))
-        self.terrain.grid(column = 0, row = 0, columnspan = 6, rowspan = 6, sticky = (N, S, E, W))
+        self.arene.grid(column = 0, row = 0, columnspan = 6, rowspan = 6, sticky = (N, S, E, W))
         play.grid(column = 6, row = 3, sticky = (W, E))
         stop.grid(column = 8, row = 3, sticky = (W, E))
+
 
         # Détermination du comportement du cadre lors des resize
         for i in range(6):
@@ -32,7 +44,7 @@ class Viewer :
             cadre.rowconfigure(i, weight = 1)
         for i in range(6, 9):
             cadre.columnconfigure(i, weight = 1)
-        
+
 
         # Essai provisoire : création d'un rectangle qui se déplace continuellement
         self.width = 20
@@ -41,27 +53,33 @@ class Viewer :
         self.y = 0
         self.dx = 2
         self.dy = 1
-        self.robot = self.terrain.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, fill = 'red', outline = 'red')
-
-        self.run()
+        self.robot = self.arene.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, fill = 'red', outline = 'red')
 
     def update(self):
         """ 
-        Permet de mettre à jour le dessin du robot 
+        Permet de mettre à jour le dessin de la toile 
         """
         self.x += self.dx 
         self.y += self.dy
 
-        self.terrain.coords(self.robot, self.x, self.y, self.x + self.width, self.y + self.height)
+        self.arene.coords(self.robot, self.x, self.y, self.x + self.width, self.y + self.height)
 
         # Permet de redessiner le terrain après un certain nombre de millisecondes
-        self.terrain.after(50, self.update)
+        self.after_id = self.arene.after(50, self.update)
 
     def run(self):
         """
         Permet de faire tourner la simulation
         """
         self.update()
+    
+    def stop(self):
+       """
+       Arrête la simulation
+       """
+       if self.after_id:
+           self.arene.after_cancel(self.after_id)
+           self.after_id = None
         
 
 simulation = Tk()
