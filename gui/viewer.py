194607ce93540d35c5a805eb4b3995198fef4c66
Angie <angie.bikou@yahoo.com>
2021-03-01 04:34:23 +0100

    Ajout de commentaires

--- gui/viewer.py
+++ gui/viewer.py
@@ -4,29 +4,36 @@ from tkinter import ttk
 
 class Viewer :
 
-    def __init__(self, simulation) : # + param arene ?
+    def __init__(self, simulation) :
 
+        # Création de la fenêtre principale de l'application
         simulation.title("Gopigo Simulator")
+        # columnconfigure et rowconfigure pour permettre à la fenêtre de s'adapter si l'utilisateur resize 
         simulation.columnconfigure(0, weight = 1)
         simulation.rowconfigure(0, weight = 1)
         
-        io = ttk.Frame(simulation)
-        terrain = Canvas(io, borderwidth = 2, relief = 'ridge', width = 600, height = 600, background = "white")
-        
-        play = ttk.Button(io, text = "Play")
-        stop = ttk.Button(io, text = "Stop")
+        # Création des widgets :
+        # Le cadre principale qui contiendra la toile, les boutons etc
+        cadre = ttk.Frame(simulation)
+        # La toile dans laquelle sera dessinée la simulation
+        terrain = Canvas(cadre, borderwidth = 2, relief = 'ridge', width = 600, height = 600, background = "white")
+        # Les boutons
+        play = ttk.Button(cadre, text = "Play")
+        stop = ttk.Button(cadre, text = "Stop")
 
-        io.grid(column = 0, row = 0, sticky = (N, S, E, W))
+        # Placement des widgets
+        cadre.grid(column = 0, row = 0, sticky = (N, S, E, W))
         terrain.grid(column = 0, row = 0, columnspan = 6, rowspan = 6, sticky = (N, S, E, W))
         play.grid(column = 6, row = 3, sticky = (W, E))
         stop.grid(column = 8, row = 3, sticky = (W, E))
 
+        # Pour déterminer le comportement du cadre lors des resize
         for i in range(6) :
-            io.columnconfigure(i, weight = 5)
+            cadre.columnconfigure(i, weight = 5)
         for i in range(6, 9) :
-            io.columnconfigure(i, weight = 1)
+            cadre.columnconfigure(i, weight = 1)
         for i in range(6) :
-            io.rowconfigure(i, weight = 1)
+            cadre.rowconfigure(i, weight = 1)
         
         terrain.create_rectangle(100, 50, 300, 250, fill = 'red', outline = 'red')
         
