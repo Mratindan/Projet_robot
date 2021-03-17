e4a06ab4a51e5c1383fb314dd06dd25d8d412cb8
PtiBaka <antony@DESKTOP-95NF774.localdomain>
2021-03-17 09:59:48 +0100

    Ajout des premiers imports et ajout des fichiers __init__.py

---
+++ modele/obstacle.py
@@ -0,0 +1,10 @@
+class Obstacle:
+	def __init__(self,largeur,hauteur):
+		self.largeur = largeur
+		self.hauteur = hauteur
+
+	def getL(self):
+		return self.largeur
+
+	def getH(self):
+		return self.hauteur
