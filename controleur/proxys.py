e451e800d891e2be5aa287ec3cc9f3232021a7f6
Angie <angie.bikou@yahoo.com>
2021-04-07 12:21:23 +0200

    Proxy_simu fonctionnel

--- controleur/proxys.py
+++ controleur/proxys.py
@@ -5,6 +5,9 @@ class Proxy_simu:
     def __init__(self, robot):
         self.robot = robot
         self.last_update = 0
+    
+    def reset_time(self):
+        self.last_update = time.time()
 
     def avance_tout_droit(self, vitesse):
         self.robot.set_vitesse(vitesse, vitesse)
@@ -15,42 +18,29 @@ class Proxy_simu:
     def tourne_gauche(self, vitesse):
         self.robot.set_vitesse(-vitesse, vitesse)
     
-    def reset_time(self):
-        self.last_update = time.time()
-
     def distance_parcourue(self, last_time):
         """
         Retourne la distance parcourue par le robot depuis la dernière mise à jour.
         """
-
-        angle = (time.time() - last_time) * self.robot.v_roue_droite
-        distance = (2 * math.pi * self.robot.diametre_roue/2 * angle) / 360
-        return distance
+        return self.robot.distance_parcourue(self.last_update)
 
     def angle_parcouru_droite(self, last_time):
         """
         Retourne l'angle parcouru vers la droite
         """
-        angle = (time.time() - last_time) * self.robot.v_roue_gauche
-        distance = (2 * math.pi * self.robot.diametre_roue/2 * angle) / 360
-        angle = (360 * distance) / (2 * math.pi * self.robot.diametre_robot/2)
-
-        return angle
+        return self.robot.angle_parcouru_droite(self.last_update)
 
     def angle_parcouru_gauche(self, last_time):
         """
         Retourne l'angle parcouru vers la gauche
         """
-        angle = (time.time() - last_time) * self.robot.v_roue_droite
-        distance = (2 * math.pi * self.robot.diametre_roue/2 * angle) / 360
-        angle = (360 * distance) / (2 * math.pi * self.robot.diametre_robot/2)
-
-        return angle
+        return self.robot.angle_parcouru_gauche(self.last_update)
     
 class Proxy_irl:
     def __init__(self, robot):
         self.robot = robot
         self.last_update = 0
+        self.last_position_robot = 0
     
     def reset_time(self):
         pass
