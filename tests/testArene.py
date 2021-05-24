14a9f4ad32813701009bbf7cbac06a81ccba764b
Aryj Takali <takaliaryj@gmail.com>
2021-05-25 00:54:08 +0200

    test unitaire de arene

--- tests/testArene.py
+++ tests/testArene.py
@@ -1,22 +1,18 @@
-from math import fabs
-import sys
+# -*- coding: utf-8 -*-
 import unittest
-import time
-import sys, os
-testdir = os.path.dirname(__file__)
-srcdir = '../modele'
-sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+from modele import Robot_simple
 from modele.arene import Arene
+
 class TestArene(unittest.TestCase):
+
     def setUp(self):
         self.test_case_est_exterieur_mur = [
-            (-1,5,Arene(30,15), True), 
-            (0,0,Arene(30,15),False), 
-            (5,-1,Arene(30,15),True),
-            (5,5,Arene(4,10),True),
-            (5,5,Arene(10,4),True),
-            (5,10,Arene(4,9),True),
-            (5,5,Arene(10,10),True)]                          
+            (-1,5,Arene(30,15,Robot_simple(200, 300),None,10,10),True),
+            (0,0,Arene(30,15,Robot_simple(200, 300),None,10,10),False),
+            (5,-1,Arene(30,15,Robot_simple(200, 300),None,10,10),True),
+            (5,5,Arene(30,15,Robot_simple(200, 300),None,10,10),False),
+            (5,10,Arene(30,15,Robot_simple(200, 300),None,10,10),False),
+            (5,5,Arene(30,15,Robot_simple(200, 300),None,10,10),False)]
 
     def test_est_exterieur_mur(self): 
         for case in self.test_case_est_exterieur_mur:
