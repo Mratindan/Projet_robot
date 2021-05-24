771bb12ff1e41d70b3dbaa2b3496a40e8a124395
Martin <martin.billon@icloud.com>
2021-05-24 19:23:39 +0200

    simulations

---
+++ simulation5.py
@@ -0,0 +1,26 @@
+from modele import Robot, Robot_simple, Arene
+from controleur import *
+from gui import Viewer
+
+def start_simulation(arene, interface_graphique) :
+    arene.controleur.start()
+    arene.start()
+    interface_graphique.lancer()
+
+# Notre robot
+wall_e=Robot(300, 300)
+
+# Notre action à donner au contrôleur
+dessine=Test3(wall_e)
+
+# Controleur
+ctrl=Controleur(dessine)
+
+# Arene
+arene=Arene(600, 600, wall_e, ctrl)
+
+# Viewer
+interface_graphique=Viewer(arene)
+
+# On lance la simulation
+start_simulation(arene, interface_graphique)
