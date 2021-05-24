0264ac6c713a6304974c69515aac470f72a646b3
Aryj Takali <takaliaryj@gmail.com>
2021-05-25 00:55:53 +0200

    test unitaire de polynome

--- tests/testPolynome.py
+++ tests/testPolynome.py
@@ -1,10 +1,20 @@
 import unittest
-from Projet_robot.modele.Polynome import Polynome
+from modele import Polynome
 
 class TestPolynome(unittest.TestCase):
+
     def setUp(self):
-        self.polynome = Polynome(2,4,6)
+        self.test_case_calcul = [
+            (2, Polynome(2,2,1), 13),
+            (2, Polynome(-1,3,1), 3),
+            (2, Polynome(0,0,0), 0),
+            (0, Polynome(-1,3,1), 1)
+        ]
+
     def test_calcul(self):
-        self.assertEqual(self.polynome.calcul(2),22)
+        for case in self.test_case_calcul:
+            polynome = case[1]
+            self.assertEquals(polynome.calcul(case[0]),case[2])
+
 if __name__ =='__main__':
     unittest.main()
