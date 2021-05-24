01a2bfd80d268ba843fdb21c4284b68855d1719b
Aryj Takali <takaliaryj@gmail.com>
2021-05-25 01:39:35 +0200

    renommer les classes/methodes

--- tests/testRobot.py
+++ tests/testRobot.py
@@ -1,18 +1,18 @@
 import unittest
 
-from modele import Robot_simple
+from modele import Robot
 
 
 class TestRobot(unittest.TestCase):
 
     def setUp(self):
         self.test_case_se_deplacer = [
-            (Robot_simple(1,1))
+            (Robot(1,1))
         ]
 
     def test_se_deplacer(self):
         for case in self.test_case_se_deplacer:
-            robot = Robot_simple(1,1)
+            robot = Robot(1,1)
             robot.set_v_roue_gauche(1)
             robot.set_v_roue_droite(3)
             with self.assertRaises(Exception):
