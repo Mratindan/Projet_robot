abd3b79b244b0fa21ed89228b3632d139dd61c79
Aryj Takali <takaliaryj@gmail.com>
2021-05-25 01:39:20 +0200

    renommer les classes/methodes

--- tests/testArene.py
+++ tests/testArene.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import unittest
-from modele import Robot_simple
+from modele import Robot
 from modele.arene import Arene
 
 class TestArene(unittest.TestCase):
 
     def setUp(self):
         self.test_case_est_exterieur_mur = [
-            (-1,5,Arene(30,15,Robot_simple(200, 300),None,10,10),True),
-            (0,0,Arene(30,15,Robot_simple(200, 300),None,10,10),False),
-            (5,-1,Arene(30,15,Robot_simple(200, 300),None,10,10),True),
-            (5,5,Arene(30,15,Robot_simple(200, 300),None,10,10),False),
-            (5,10,Arene(30,15,Robot_simple(200, 300),None,10,10),False),
-            (5,5,Arene(30,15,Robot_simple(200, 300),None,10,10),False)]
+            (-1,5,Arene(30,15,Robot(200, 300),None,10,10),True),
+            (0,0,Arene(30,15,Robot(200, 300),None,10,10),False),
+            (5,-1,Arene(30,15,Robot(200, 300),None,10,10),True),
+            (5,5,Arene(30,15,Robot(200, 300),None,10,10),False),
+            (5,10,Arene(30,15,Robot(200, 300),None,10,10),False),
+            (5,5,Arene(30,15,Robot(200, 300),None,10,10),False)]
 
     def test_est_exterieur_mur(self): 
         for case in self.test_case_est_exterieur_mur:
